1153810c5def8c4490fd3a5766327255103c7e49
LucasA21 <lucasaraya19@hotmail.com>
2024-11-06 23:29:19 -0300

    feat: :sparkles: Se creo el modulo Inventory y su CRUD

---
+++ src/api/v1/inventory/infrastructure/http/dtos/pydantic_create_item_dto.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+from src.api.v1.inventory.application.create.create_item_dto import CreateItemDto
+
+class PydanticCreateItemDto(BaseModel):
+    user_id : str
+    product_name : str
+    amount: int
+    expiration_date: date
+
+    def to_application(self) -> CreateItemDto:
+        return CreateItemDto(
+            user_id=self.user_id,
+            product_name=self.product_name,
+            amount=self.amount,
+            expiration_date=self.expiration_date
+        )
---
+++ src/api/v1/inventory/infrastructure/http/dtos/pydantic_update_item_dto.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+from src.api.v1.inventory.application.update.update_item_dto import UpdateItemDto
+
+class PydanticUpdateItemDto(BaseModel):
+    inventory_id: str
+    product_name: str
+    amount: int
+    expiration_date: date
+
+    def to_aplication(self) -> UpdateItemDto:
+        return UpdateItemDto(
+            inventory_id=self.inventory_id,
+            product_name=self.product_name,
+            amount=self.amount,
+            expiration_date=self.expiration_date
+        )
